4765a8ea05b43440c70e8ae7e132806004eaf1af
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-01-17 12:42:11 -0800

    footer navbar members added

---
+++ src/components/Members.jsx
@@ -0,0 +1,166 @@
+import { Stack, Typography} from '@mui/material';
+import Avatar from '@mui/material/Avatar';  
+import React from 'react';
+
+function Members() {
+  return (
+    <Stack sx={{ gap: "20px", padding: "20px", justifyContent: "space-between", mt: "500px", mb: "25px" }}>
+      <Typography>Administration</Typography>
+
+      <Stack
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { xs: '1fr', md: 'repeat(4, 1fr)' },
+          gap: { xs: '20px', md: '10px' },
+        }}
+      >
+        {/* Member 4 */}
+        <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+              {/* Member 4 */}
+              <Stack
+          sx={{
+            gap: "5px",
+            bgcolor: "white",
+            border: "1px solid black",
+            borderRadius: "8px",
+            minWidth: "200px",
+            justifyContent: "center",
+            padding: "10px",
+            mb: "20px",
+          }}
+        >
+          <Avatar alt="Prof. Fourth Member" src="/majhi.jpeg" sx={{ width: 55, height: 55, ml:"5px" }} />
+          <Typography sx={{ fontWeight: "bold", ml: "5px" }}>PROF.FOURTH MEMBER</Typography>
+          <Typography sx={{ml:"5px"}}>Fourth Member Role</Typography>
+        </Stack>
+
+      </Stack>
+    </Stack>
+  );
+}
+
+export default Members;
